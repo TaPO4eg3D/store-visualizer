a81aef2f174f4b7722b957dbca21d0309c95818a
Denis Fomin <fo.denis2011@gmail.com>
2022-10-04 20:16:19 +0300

    play around with bevy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,74 @@
-use store_visualizer::run;
+use bevy::prelude::*;
+use store_visualizer::camera::*;
 
 fn main() {
-    pollster::block_on(run());
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(FlyCameraPlugin)
+        .add_system(cursor_grab_system)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn cursor_grab_system(
+    mut windows: ResMut<Windows>,
+    btn: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+) {
+    let window = windows.get_primary_mut().unwrap();
+
+    if btn.just_pressed(MouseButton::Left) {
+        window.set_cursor_lock_mode(true);
+        window.set_cursor_visibility(false);
+    }
+
+    if key.just_pressed(KeyCode::Escape) {
+        window.set_cursor_lock_mode(false);
+        window.set_cursor_visibility(true);
+    }
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    
+    let cube_texture = asset_server.load("./cube.jpeg");
+
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+     let cube_material = materials.add(StandardMaterial {
+        base_color_texture: Some(cube_texture.clone()),
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    });
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: cube_material,
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn()
+        .insert_bundle(Camera3dBundle::default())
+        .insert(FlyCamera::default());
 }
